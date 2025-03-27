c2281ad518c8593cbcf43451821b66363a4d3401
Vipul J Patil <vipulpatil2500@gmail.com>
2025-03-27 08:10:41 +0530

    Delete backend directory

--- backend/config.py
+++
@@ -1,4 +0,0 @@
-import os
-
-UPLOAD_FOLDER = "uploads"
-SECRET_KEY = "your_secret_key"
--- backend/test.py
+++
@@ -1,5 +0,0 @@
-from app.pdf_processor import extract_text_from_pdf
-
-pdf_path = "uploads/llm.pdf"
-extracted_text = extract_text_from_pdf(pdf_path)
-print(extracted_text)  # Check if the text is extracted correctly
